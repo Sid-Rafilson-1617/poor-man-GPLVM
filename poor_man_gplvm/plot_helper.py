0f0f4c2d21efa9b3497f2121a33e29cfc9c7670d
Sam Zheng <samzhenguchi@gmail.com>
2025-08-11 15:34:00 -0400

    plot_helper

---
+++ poor_man_gplvm/plot_helper.py
@@ -0,0 +1,19 @@
+'''
+helper functions for plotting
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pynapple as nap
+import sys,os
+
+def save_fig(fig,fig_name,fig_dir='./figs',fig_format=['png','svg'],dpi=300):
+    '''
+    save figure to fig_dir
+    '''
+    if not os.path.exists(fig_dir):
+        os.makedirs(fig_dir)
+    for fmt in fig_format:
+        fig.savefig(os.path.join(fig_dir,fig_name+f'.{fmt}'),dpi=dpi)
+        print(f'saved {fig_name}.{fmt} to {fig_dir}')
+    plt.close(fig)
