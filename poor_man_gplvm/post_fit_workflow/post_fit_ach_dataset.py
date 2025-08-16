4a08fc55d2be2efe6d96f2ed9cf51183f701062c
Sam Zheng <samzhenguchi@gmail.com>
2025-08-16 16:39:12 -0400

    fix

--- poor_man_gplvm/post_fit_workflow/post_fit_ach_dataset.py
+++ poor_man_gplvm/post_fit_workflow/post_fit_ach_dataset.py
@@ -25,14 +25,15 @@ import pynapple as nap
 
 
 # helper function to get list of processed decoding result from em_res_l (multiple em fit results)
-def get_decode_res_l_from_em_res_l(em_res_l,load_res):
+def get_decode_res_l_from_em_res_l(em_res_l,t_l=None):
     decode_res_l = []
     for em_res in em_res_l:
         log_posterior_final=em_res['log_posterior_final']
         post_latent_marg = np.exp(scipy.special.logsumexp(log_posterior_final,axis=1))
         post_dynamics_marg = np.exp(scipy.special.logsumexp(log_posterior_final,axis=2))
-        
-        decode_res_one = {'posterior_latent_marg':nap.TsdFrame(d=post_latent_marg,t=load_res['t_l']),'posterior_dynamics_marg':nap.TsdFrame(d=post_dynamics_marg,t=load_res['t_l'])}
+        if t_l is None:
+            t_l = np.arange(post_latent_marg.shape[0])
+        decode_res_one = {'posterior_latent_marg':nap.TsdFrame(d=post_latent_marg,t=t_l),'posterior_dynamics_marg':nap.TsdFrame(d=post_dynamics_marg,t=t_l)}
         
         decode_res_l.append(decode_res_one)
     return decode_res_l
