4fa3dc24c792337ad991eae016a8660535f111ab
Sam Zheng <samzhenguchi@gmail.com>
2025-08-20 19:27:58 -0400

    fix

--- poor_man_gplvm/post_fit_workflow/post_fit_ach_dataset.py
+++ poor_man_gplvm/post_fit_workflow/post_fit_ach_dataset.py
@@ -711,13 +711,15 @@ def latent_cluster_vs_timing_regression(cluster_label_l,event_ts,nrem_intv,do_pr
             shuffle_params_l.append(shuffle_reg_res.params)
             shuffle_prsquared_l.append(shuffle_reg_res.prsquared)
         
-        shuffle_res_d['t_l'] = pd.concat(shuffle_t_l,axis=0)
-        shuffle_res_d['p_l'] = pd.concat(shuffle_p_l,axis=0)
-        shuffle_res_d['params_l'] = pd.concat(shuffle_params_l,axis=0)
+        shuffle_res_d['t_l'] = np.stack(shuffle_t_l,axis=0)
+        shuffle_res_d['p_l'] = np.stack(shuffle_p_l,axis=0)
+        shuffle_res_d['params_l'] = np.stack(shuffle_params_l,axis=0)
         shuffle_res_d['prsquared_l'] = np.array(shuffle_prsquared_l)
         shuffle_res_d['prsquared_high'] = np.quantile(shuffle_res_d['prsquared_l'],quantile,axis=0)
-        # shuffle_res_d['t_high'] = shuffle_res_d['t_l'].groupby(level=1).apply(lambda x: np.quantile(x,quantile,axis=0))
-        # shuffle_res_d['p_high'] = shuffle_res_d['p_l'].groupby(level=1).apply(lambda x: np.quantile(x,quantile,axis=0))
+        shuffle_res_d['t_high'] = np.quantile(shuffle_res_d['t_l'],quantile,axis=0)
+        shuffle_res_d['t_high'] = pd.DataFrame(shuffle_res_d['t_high'],index=shuffle_reg_res.tvalues.index,columns=shuffle_reg_res.tvalues.columns)
+        shuffle_res_d['p_high'] = np.quantile(shuffle_res_d['p_l'],quantile,axis=0)
+        shuffle_res_d['p_high'] = pd.DataFrame(shuffle_res_d['p_high'],index=shuffle_reg_res.pvalues.index,columns=shuffle_reg_res.pvalues.columns)
         res['shuffle_res_d'] = shuffle_res_d
             
     return res
