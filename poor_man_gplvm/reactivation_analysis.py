458d1239e1d16d6269e6ab44328b05a5726eefc0
Sam Zheng <samzhenguchi@gmail.com>
2025-09-18 17:21:33 -0400

    fix

--- poor_man_gplvm/reactivation_analysis.py
+++ poor_man_gplvm/reactivation_analysis.py
@@ -163,7 +163,7 @@ def aggregate_within_ripple_per_epoch(posterior_latent,ripple_intv_per_ep,):
     post_agg_within_ripple_mean_ep_d = {}
     for ep,intv in ripple_intv_per_ep.items():
         posterior_latent_ep = posterior_latent.restrict(intv)
-        coords ={'time':tsdtensor.t}
+        coords ={'time':posterior_latent_ep.t}
         rip_label = intv.in_interval(posterior_latent_ep)
         posterior_latent_ep_xr = xr.DataArray(posterior_latent_ep.d,dims=dims,coords=coords)
         posterior_latent_ep_xr = posterior_latent_ep_xr.assign_coords(ripple_label=('time',rip_label))
